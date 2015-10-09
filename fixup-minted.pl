552fab7c3375fc28dee5ff778d098e2ccc754ff4
Ingo Blechschmidt <iblech@web.de>
2015-10-09 22:47:45 +0200

    Gescheiterter Versuch schöner Unicode-Zeichen

---
+++ fixup-minted.pl
@@ -0,0 +1,8 @@
+#!/usr/bin/perl -i -p
+
+use warnings;
+use strict;
+
+s/\\PYGdefault{err}{∷}/\\PYGdefault{o+ow}{∷}/g;
+s/\\PYGdefault{err}{⇒}/\\PYGdefault{o+ow}{⇒}/g;
+s/\\PYGdefault{err}{→}/\\PYGdefault{o+ow}{→}/g;
